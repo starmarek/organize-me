8db8c97ee21f75fc58700df26a1cac677ca3b344
Aleksander Pucher <pucher.aleksander@gmail.com>
2020-12-28 22:29:22 +0100

    fix heroku static files bug

--- backend/settings.py
+++ backend/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-SETTINGS_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(SETTINGS_DIR)
 
 
