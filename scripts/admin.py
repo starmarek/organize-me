85d41f2866d2881504842db30e1d23de52558959
Aleksander Pucher <pucher.aleksander@gmail.com>
2021-01-09 19:18:49 +0100

    add gitlab-ci forattin to admin script

--- scripts/admin.py
+++ scripts/admin.py
@@ -8,16 +8,22 @@ from pathlib import Path
 import coloredlogs
 import dotenv
 import fire
+import ruamel.yaml
 import toml
 
 log = logging.getLogger("admin")
 coloredlogs.install(level="DEBUG")
 
 dotenv_file = dotenv.find_dotenv()
+dotenv_template = dotenv.find_dotenv(".template.env")
 dotenv.load_dotenv(dotenv_file, verbose=True, override=True)
 with open("Pipfile", "r") as f:
     pipfile = toml.load(f, _dict=dict)
-
+with open(".gitlab-ci.yml", "r") as f:
+    gitlab_ci, sequence, block = ruamel.yaml.util.load_yaml_guess_indent(f, preserve_quotes=True)
+yaml = ruamel.yaml.YAML()
+yaml.width = 4096
+yaml.indent(mapping=block, sequence=sequence, offset=block)
 
 # def assert_python_package(package, to_assert):
 #     # assert platform.python_version() == os.environ["PYTHON_VER"], "Please use proper version of python"
@@ -33,21 +39,30 @@ with open("Pipfile", "r") as f:
 # assert version("pipenv") == os.environ["PIPENV_VER"], "Please use proper version of pipenv package"
 
 
-# def update_python(ver):
-#     dotenv.set_key(dotenv_file, "PYTHON_VER", ver, "")
-#     pipfile["requires"]["python_version"] = ver
-#     with open("Pipfile", "r+") as f:
-#         toml.dump(pipfile, f)
-#     os.environ["PIPENV_DONT_USE_PYENV"] = "1"
-#     output = subprocess.run(
-#         ["pipenv", "update", "--keep-outdated", "--python", ver], capture_output=True, text=True, check=True
-#     )
-#     print(output.stdout)
-#     print(output.stderr)
+def update_python(ver, vscode=False):
+    log.info("Running update_python command")
+
+    dotenv.set_key(dotenv_file, "CORE_PYTHON_VER", ver, "")
+    dotenv.set_key(dotenv_template, "CORE_PYTHON_VER", ver, "")
+
+    pipfile["requires"]["python_version"] = ver
+    with open("Pipfile", "r+") as f:
+        toml.dump(pipfile, f)
+
+    gitlab_ci["variables"]["PYTHON_VERSION"] = ver
+    with open(".gitlab-ci.yml", "r+") as f:
+        yaml.dump(gitlab_ci, f)
+
+    subprocess.run(["pipenv", "--rm"], check=True)
+    subprocess.run(["pipenv", "update", "--keep-outdated", "--dev"], check=True)
+
+    if vscode:
+        _update_virtualenv_vscode_pythonpath()
 
 
 def build_containers(cache=True):
     log.info("Running build-containers command")
+
     subprocess.run(
         shlex.split(f"docker-compose build --force-rm --parallel {'' if cache else '--no-cache'}"), check=True
     )
@@ -55,6 +70,7 @@ def build_containers(cache=True):
 
 def run_containers():
     log.info("Running run-containers command")
+
     subprocess.run(shlex.split("docker-compose up --detach --remove-orphans --force-recreate"), check=True)
 
 
@@ -82,6 +98,7 @@ def _update_virtualenv_vscode_pythonpath():
 
 def init(vscode=False):
     log.info("Running init command")
+
     build_containers(cache=False)
     run_containers()
     if vscode:
