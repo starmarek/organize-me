56924d855380717da822e6ade0853af214905791
Aleksander Pucher <pucher.aleksander@gmail.com>
2021-01-09 12:23:22 +0100

    run schellcheck over admin.sh

--- admin.sh
+++ admin.sh
@@ -2,26 +2,27 @@
 
 
 if [[ -f .env ]]; then
-    export $(cat .env | sed 's/#.*//g'| xargs)
+    # shellcheck disable=SC2046
+    export $(sed 's/#.*//g' .env | xargs)
 else
     echo -e "\e[91mERROR: You need to create your .env file first!\e[0m"
-    exit
+    exit 1
 fi
 
 
-pipenv_version=($(pipenv --version 2> /dev/null))
-if [[ -z "${pipenv_version[2]}" ]]; then
+IFS=" " read -r -a pipenv_version_array <<< "$(pipenv --version 2> /dev/null)"
+if [[ -z "${pipenv_version_array[2]}" ]]; then
     echo -e "\e[91mERROR: You need to install pipenv first!\e[0m"
-    exit
-elif [[ "${pipenv_version[2]}" != "${CORE_PIPENV_VER}" ]]; then
+    exit 1
+elif [[ "${pipenv_version_array[2]}" != "${CORE_PIPENV_VER}" ]]; then
     echo -e "\e[33mWARNING: We highly recommend to use tested pipenv version --> ${CORE_PIPENV_VER}\e[0m"
 fi
 
 
 if ! pipenv --venv &> /dev/null; then
-    pipenv sync --dev
+    pipenv sync --dev || exit 1
 fi
 
 
 python_path=$(pipenv run which python 2> /dev/null)
-${python_path} scripts/admin.py $@
+${python_path} scripts/admin.py "$@"
